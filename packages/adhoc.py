8734eb926d865e6b4c1144bd1e553b50a6ace2a6
Shijia Shu <shu_shijia@outlook.com>
2018-05-20 16:47:00 -0700

    Fix the bug in max_histogram_area. Modified the jupyter notebook.

--- packages/adhoc.py
+++ packages/adhoc.py
@@ -2,6 +2,9 @@
 Ad-Hoc Communications Network Installation
 """
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
 
 def generate_region(length, width):
     """
@@ -110,9 +113,8 @@ def max_histogram_area(histogram, row):
     max_loc = dict()
     stack = []
     for i in xrange(len(histogram) + 1):
-        if i == 0:
-            stack.append(i)
-        elif i == len(histogram) or histogram[i] < histogram[i - 1]:
+        if i == 0: stack.append(i)
+        elif i == len(histogram):
             while len(stack) != 0:           
                 top = stack.pop()   
                 if len(stack) == 0:
@@ -125,10 +127,25 @@ def max_histogram_area(histogram, row):
                     max_loc['loc'] = (row - histogram[top] + 1, i - w) # Not Sure !!!!
                     max_loc['length'] = l
                     max_loc['width'] = w
+        elif histogram[i] < histogram[i - 1]:
+            while len(stack) != 0 and histogram[stack[-1]] >= histogram[i]:           
+                top = stack.pop()   
+                if len(stack) == 0:
+                    l, w = histogram[top], i
+                else:
+                    l, w = histogram[top], (i - stack[-1] - 1)
+                rect_area = l * w 
+                if rect_area > max_rect:
+                    max_rect = rect_area
+                    max_loc['loc'] = (row - histogram[top] + 1, i - w) # Not Sure !!!!
+                    max_loc['length'] = l
+                    max_loc['width'] = w
+                if len(stack) == 0: break
             stack.append(i)
         else: 
             stack.append(i)
             # print stack
+
     return max_rect, max_loc
 
 
@@ -150,17 +167,19 @@ def find_max_rect(region):
     hist_region[:] = region[:]
     #print hist_region
     max_rect, max_loc = max_histogram_area(hist_region[0, :], 0)
-
+    #print "histogram", hist_region[0,:]
     for i in range(1, l):
         for j in range(w):
             if hist_region[i][j] != 0:
-                hist_region[i][j] = hist_region[i - 1][j] + 1
+                hist_region[i][j] += hist_region[i - 1][j]
             else:
                 hist_region[i][j] = 0
         
         tmp_max_rect, tmp_max_loc = max_histogram_area(hist_region[i,:], i)
+        #print "histogram", hist_region[i,:]
         if tmp_max_rect > max_rect:
             max_rect, max_loc = tmp_max_rect, tmp_max_loc
+            #print max_rect, i, j
 
     return max_rect, max_loc
 
@@ -183,8 +202,8 @@ def trim_tower(region, tower, show_coverage_update=False):
      # To prepare for finding the largest area of rectangle
     patch = np.zeros((l, w), dtype=int)
     patch[:] = region[loc[0] : loc[0] + l, loc[1] : loc[1] + w]
-    patch[patch == 1] = 1 # 1 represents open space
-    patch[patch > 1] = 0 # 0 represents used space
+    #patch[patch == 1] = 1 # 1 represents open space
+    patch[patch != 1] = 0 # 0 represents used space
     # Find the largest area of rectangle
     tower_area, tmp_tower_loc = find_max_rect(patch)
     # To calculate the new tower coverage
@@ -192,7 +211,7 @@ def trim_tower(region, tower, show_coverage_update=False):
     tower_new['loc'] = (tmp_tower_loc['loc'][0] + loc[0], tmp_tower_loc['loc'][1] + loc[1])
     tower_new['length'] = tmp_tower_loc['length']
     tower_new['width'] = tmp_tower_loc['width']
-    print "Trimed Tower Area: ", tower_area
+    #print "Tower Area after trimming: ", tower_area
     
     if show_coverage_update == True:
         new_patch = np.zeros((l, w), dtype=int)
@@ -200,16 +219,34 @@ def trim_tower(region, tower, show_coverage_update=False):
         # Add the new coverage on a plane region map
         coverage_update_display = np.zeros(region.shape, dtype=int)
         coverage_update_display[loc[0] : loc[0] + l, loc[1] : loc[1] + w] = new_patch
-        print "The new tower coverage location info: ", tower_new
-        print "The trimmed tower coverage: \n", coverage_update_display
+        #print "The new tower coverage location info: ", tower_new
+        #print "The trimmed tower coverage: \n", coverage_update_display
 
-        
     return tower_area, tower_new
 
-    
+def full_coverage_generation(region, display=False, debug=False):
+    target_area = region.shape[0] * region.shape[1]
+
+    tower_num_need = 0
+    coverage_area = 0
+
+    while coverage_area != target_area:
+            # Generate the new tower to be installed into the region
+            tower = generate_tower(region.shape)
+            # 
+            region_update = place_tower(region, tower)
+            # To keep the maximum possible coverage
+            tower_area, tower_loc_new = trim_tower(region_update, tower)
+            if tower_area != 0:
+                tower_num_need += 1
+                coverage_area  += tower_area
+            # To update the region
+            region = place_tower(region, tower_loc_new)
+
+    return tower_num_need, coverage_area
 
    
-def coverage_generation(region, target_area, tower_count=None, display=False):
+def coverage_generation(region, tower_num, display=False, debug=False):
     # Generate the coverage map by inputing the desired coverage and a sequence of n  communication towers. 
     """
     Need to be added
@@ -222,21 +259,62 @@ def coverage_generation(region, target_area, tower_count=None, display=False):
         coverage_area (int): The total area of the coverage after execution
         coverage_map (<numpy.ndarray>): The updated region map with the new towers added.
     """
-    # coverage_area = 0
+    target_area = region.shape[0] * region.shape[1]
+    print target_area
+    coverage_area = 0
+    tower_cnt = 0
+    if display == True: fig, ax = draw_region(region.shape)
+    tower_loc_list = []
+    while tower_num != tower_cnt:
+            # Generate the new tower to be installed into the region
+            tower = generate_tower(region.shape)
 
-    # if tower_count == None:
-    #     while coverage_area != target_area:
-    #         # Generate the new tower to be installed into the region
-    #         tower = generate_tower(region.shape)
-    #         # 
-    #         region_update = place_tower(region, tower)
-    #         trimmed_tower, tower_area, tower_loc_new = trim_tower(region_update, tower)
-    #         update_tower
-    
-    pass
+            if debug: print "The tower generated initially: \n", tower
+            # 
+            region_update = place_tower(region, tower)
+            if debug: print "The region after inital tower placement: \n", region_update
+            # To keep the maximum possible coverage
+            tower_area, tower_loc_new = trim_tower(region_update, tower, show_coverage_update=debug)
+
+            # To update the region
+            if tower_area > 0: 
+                tower_cnt += 1   
+                tower_loc_list.append(tower_loc_new)
+                if display == True: ax = draw_tower(ax, tower_loc_new)     
+                region = place_tower(region, tower_loc_new)
+                if np.sum(region[region > 1]) != 0:
+                    print "Original_tower: ", tower
+                    print tower_loc_new
+                    print tower_loc_list
+                    raise STOP
+            coverage_area  += tower_area
+
+            if coverage_area >= target_area:
+                print "Coverage requirement met, ", coverage_area
+                break
+    print "Total tower used: ", tower_cnt
+    percent_coverage = (coverage_area * 1.0 / target_area) * 100
+    print "The overall percentage of coverage area: ", percent_coverage, "%"
+    return coverage_area, region, percent_coverage, tower_loc_list
 
 
+def draw_region(region_size):
+    fig, ax = plt.subplots(1)
+    print region_size[0]
+    plt.ylim(region_size[0],0)
+    plt.xlim(0,region_size[1])
+    #ax.grid(color='r', linestyle='-', linewidth=1)
+    return fig, ax
 
+def draw_tower(ax, tower, color_selection=None):
+    l, w = tower['length'], tower['width']
+    loc_xy = (tower['loc'][1], tower['loc'][0])
+    if color_selection == None:
+        color_selection = (np.random.uniform(0,1), np.random.uniform(0,1), np.random.uniform(0,1))
+        #color_selection = 'b'
+
+    ax.add_patch(Rectangle(xy=loc_xy, height=l, width=w, color=color_selection, hatch='\\', ec=(0,0,0,1)))
+    return ax
 
 
 
